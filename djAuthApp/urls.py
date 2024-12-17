9e72f7d88296b721b765911b46366c9ad98beecf
mivehk <k.mivehnejad@gmail.com>
2024-12-17 16:21:15 -0500

    redirect generics routing of djAuthApp/ to register view

--- djAuthApp/urls.py
+++ djAuthApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+	path('', RedirectView.as_view(pattern_name='register', permanent=False)),
     path('register/', views.register, name='register'),  # User registration
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Login view
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),  # Logout view
--- djAuthProj/urls.py
+++ djAuthProj/urls.py
@@ -15,25 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-"""
-from django.contrib import admin
-from django.urls import path , include #imported include to add djAuthApp url
-
-from django.contrib.auth import views as auth_views
-
-## i used a separate script to add project directory to sys.path
-##PosixPath('/Users/kmive/Desktop/python/django_Auth_Project/djAuthProj')
-from djAuthApp import views 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djAuthApp/', include('django.contrib.auth.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('register/', views.register, name='register'),
-]"""
-
 from django.contrib import admin
 from django.urls import path , include
 
